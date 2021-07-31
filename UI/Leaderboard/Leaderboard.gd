d773e824f31f0bba1483ec8476ca2dc08b4a7287
Lucas Becker <LucasBeckerPro@gmail.com>
2021-07-31 20:00:00 +0100

    The leaderboard is populated from the entries in the AWS DynamoDB.

--- UI/Leaderboard/Leaderboard.gd
+++ UI/Leaderboard/Leaderboard.gd
@@ -13,13 +13,16 @@ func _ready():
 	
 	#$VBoxContainer/HTTPRequest.connect("request_completed", self, "_on_request_completed")
 	
+	"""
 	for i in range(20):
 		var entry = ScoreEntry.instance()
 		entry.text = "Player %s" % i
 		$VBoxContainer.add_child(entry)
 		
 		#print(get_node("Label").text) 
+	"""
 
+	$VBoxContainer/HTTPRequest.request("https://cqdzwos026.execute-api.eu-west-1.amazonaws.com/items")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,20 +36,29 @@ func _on_Button_pressed():
 	print(get_node("VBoxContainer/Label").text)
 	
 	pass # Replace with function body.
-"""
 
 
 func _on_Button_pressed():
 	
-	$VBoxContainer/HTTPRequest.request("http://api.webhookinbox.com/i/yO1v9xvx/in/")
+	$VBoxContainer/HTTPRequest.request("https://cqdzwos026.execute-api.eu-west-1.amazonaws.com/items")
 	print("Ouiiii")
-	
+"""
 
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	
-	#var json = JSON.parse(body.get_string_from_utf8())
-	#print(json.result)
+	var json = JSON.parse(body.get_string_from_utf8())
+	#print(json.result["Items"])
 	
-	print(body.get_string_from_utf8())
+	for elt in json.result["Items"]:
+		
+		var entry = ScoreEntry.instance()
+		entry.text = "Player %s" % elt
+		$VBoxContainer.add_child(entry)
+		
+		print(elt)
+	print(json.result)
+	
+	
+	#print(body.get_string_from_utf8())
 	
