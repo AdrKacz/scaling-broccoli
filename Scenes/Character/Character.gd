f79d7f24f5a5572d670613ce662e7575ef26b215
AdrKacz <adrien.kaczmarek@gmail.com>
2021-07-31 17:42:25 +0100

    Button to Area

--- Scenes/Character/Character.gd
+++ Scenes/Character/Character.gd
@@ -4,9 +4,11 @@ signal click
 var state = 0
 
 
-func _on_TextureButton_pressed():
-	emit_signal("click", state)
-
 func set_state(not_this_state):
 	state = Constants.new_state([state, not_this_state])
 	$State.text = str(state)
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventScreenTouch:
+		emit_signal("click", state)
