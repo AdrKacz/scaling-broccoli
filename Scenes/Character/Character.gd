6f39f2bf347c775a909e25311adbff066d6a3b3e
Escaron Tanguy <tanguy.escaron@student-cs.fr>
2021-07-31 18:45:44 +0100

    press with space

--- Scenes/Character/Character.gd
+++ Scenes/Character/Character.gd
@@ -11,6 +11,10 @@ func set_state(backgroud_state):
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventScreenTouch and event.is_pressed():
 		emit_signal("click", state)
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_SPACE:
+		emit_signal("click", state)
+		
 		
 func change_state(new_state):
 	$State.text = str(state)
