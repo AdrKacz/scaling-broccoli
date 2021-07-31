8577899ff60303f1c04687267fa86af5b0e5887b
AdrKacz <adrien.kaczmarek@gmail.com>
2021-08-01 00:12:25 +0100

    Wick

--- AutoLoad/Constants/constants.gd
+++ AutoLoad/Constants/constants.gd
@@ -4,6 +4,8 @@ extends Node
 const can_be_background_state = true
 const background_wait_time = 0.4
 
+const max_time = 30
+
 
 # ===== ===== =====
 enum StateEnum {
--- Scenes/TimeTrial/TimeTrial.gd
+++ Scenes/TimeTrial/TimeTrial.gd
@@ -1,22 +1,46 @@
-extends Node2D
+extends Node
 signal lost
 
-var time = 20
-var perdu = false
+const ratio = 1280 / 720
+
+const max_wick = 400;
+
+var static_time = Constants.max_time
+var animated_time = Constants.max_time
+
+var first_quarter = Constants.max_time / 4
+var second_quarter = first_quarter * 2
+var third_quarter = first_quarter * 3
 
 func add_time(dt):
-	time += dt
-	$TempsText.text = str(time)
+	update_time(static_time + dt)
 	
 func remove_time(dt):
-	time -= dt
-	$TempsText.text = str(time)
+	update_time(static_time - dt)
+	
+func _process(delta):
+#	Update wick
+	var interpolation = lerp(0, max_wick, animated_time / Constants.max_time)
+	$Control/TextureProgressUp.value = interpolation
+	$Control/TextureProgressRight.value = interpolation
+	$Control/TextureProgressDown.value = interpolation
+	$Control/TextureProgressLeft.value = interpolation
+	
+#	Check if no time left and emit signal
+#	TODO
+	if animated_time <= 0:
+		print("... No time left")
+		Session.lose()
+	
+func update_time(new_static_time):
+#	Tween for interpolation
+	static_time = min(new_static_time, Constants.max_time)
+	$Tween.interpolate_property(self, "animated_time", animated_time, static_time, $UpdateUI.wait_time)
+	if not $Tween.is_active():
+		$Tween.start()
+	
 
 func _on_UpdateUI_timeout():
-	time -= 1
-	$TempsText.text = str(time)
+	print("... Timeout")
+	update_time(static_time - $UpdateUI.wait_time)
 	
-	if time <= 0:
-		print("LOST")
-		$TempsText.text = "PERDU!"
-		Session.lose()
