1ff3d2d993762bfa5ac75fc2e20785af57a67f4e
Lucas Becker <LucasBeckerPro@gmail.com>
2021-07-31 17:05:32 +0100

    First tests with Godot and the leaderboard. Added a button to understand Signals

---
+++ UI/Leaderboard/Leaderboard.gd
@@ -0,0 +1,24 @@
+extends ScrollContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Wsh mec")
+	
+	#print(get_node("Label").text) 
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	print("Oui monsieur")
+	pass # Replace with function body.
