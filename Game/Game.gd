8f443ed1f60523a7b90b5e4ae383bbbd0ca64559
Escaron Tanguy <tanguy.escaron@student-cs.fr>
2021-08-01 02:04:35 +0100

    multi game experience

--- Game/Game.gd
+++ Game/Game.gd
@@ -1,31 +1,31 @@
 extends Node2D
+
 export (PackedScene) var ComboScene
 export var wait_time = Constants.background_wait_time
 
-var combo = 0
-var score = 0
+signal score
+signal miss
+signal wrong
 
 func _ready():
 	# Starts randomly not to be at the same time than other games
 	yield(get_tree().create_timer((randi() % int(100 * wait_time) / 100)), "timeout")
 	$ChangeState.wait_time = wait_time
-	$ComboTimer.wait_time = Constants.max_combo_time # NOTE Can be infinite
 
 	$ChangeState.start()
 	$Background.change_state(0)
 	$Character.set_state(1)
 
-# Every time the player clicks we check
+# Every time the player clicks
 func _on_Character_click(state):
-	# He won
+	# He scored
 	if $Character.state == $Background.state:
-		score += 1 * max(1, combo)
-		print("SCORE : ", score)
-#		$TimeTrial.add_time(1 * combo) # NOTE To parametize
-		add_combo()
+		emit_signal("score")
 
+		# Reset timer
 		$ChangeState.stop()
-
+		
+		# Update background and character
 		var new_background_state = Constants.new_state([$Background.state])
 		$Background.change_state(new_background_state)
 		$Character.set_state(new_background_state)
@@ -34,42 +34,20 @@ func _on_Character_click(state):
 
 	# He lost
 	else:
+		emit_signal("wrong")
 		$Character.wrong_color()
-		reset_combo()
-		add_combo()
 
 	# Reset character's state that isn't the background value
 	print("Click on state ", state)
 
 
-# Every background change we check if we miss
 func _on_ChangeState_timeout():
+	# Every background change we check if we miss
 	if $Background.state == $Character.state:
-		print("... Miss background ")
-#		reset_combo() # NOTE To parametize
+		emit_signal("miss")
+	# Update background
 	var new_background_state = Constants.new_state([$Background.state])
 	$Background.change_state(new_background_state)
 
-func reset_combo():
-#	$TimeTrial.remove_time(1) # NOTE To parametize
-#	TODO : Animation to see
-	$ComboTimer.stop()
-	combo = 0
-
-func add_combo():
-	combo += 1
-	if combo >= 2:
-		var comb = ComboScene.instance()
-		comb.multiplier = combo
-		print()
-		var x = $CornersCombo/UpperLeft.position.x + randi() % int($CornersCombo/LowerRight.position.x - $CornersCombo/UpperLeft.position.x)
-		var y = $CornersCombo/UpperLeft.position.y + randi() % int($CornersCombo/LowerRight.position.y - $CornersCombo/UpperLeft.position.y)
-		comb.position = Vector2(x, y)
-		add_child(comb)
-#		Add Combo Timer # NOTE To parametize
-		$ComboTimer.stop()
-		$ComboTimer.start()
 
 
-func _on_ComboTimer_timeout():
-	reset_combo()
--- Game/GameMaster.gd
+++ Game/GameMaster.gd
@@ -1,9 +1,79 @@
 extends Node2D
 
+export (PackedScene) var Game
 export (PackedScene) var ComboScene
 
 var score = 0
 var combo = 0
 var timer = 30
+# Number of active games
+var mode = 6
+var modes = [1, 2, 4, 6]
+
+func _ready():
+	$ComboTimer.wait_time = Constants.max_combo_time # NOTE Can be infinite
+	setup_games()
+
+func score():
+	score += max(1, combo)
+	$TimeTrial.add_time(1)
+	add_combo()
+
+func miss():
+#	$TimeTrial.remove_time(1) # NOTE To parametize
+#	TODO : Animation to see
+	$ComboTimer.stop()
+	combo = 0
+
+func wrong():
+	$TimeTrial.remove_time(2)
+	miss()
+	
+
+func add_combo():
+	combo += 1
+	# Display combo
+	if combo >= 2:
+		var comb = ComboScene.instance()
+		comb.multiplier = combo
+		var x = $CornersCombo/UpperLeft.position.x + randi() % int($CornersCombo/LowerRight.position.x - $CornersCombo/UpperLeft.position.x)
+		var y = $CornersCombo/UpperLeft.position.y + randi() % int($CornersCombo/LowerRight.position.y - $CornersCombo/UpperLeft.position.y)
+		comb.position = Vector2(x, y)
+		add_child(comb)
+#		Add Combo Timer # NOTE To parametize
+		$ComboTimer.stop()
+		$ComboTimer.start()
+	# Change mode
+	if combo >= 3:
+		combo = 0
+		change_mode()
+
+func change_mode():
+	var new_mode = modes[randi() % modes.size()]
+	while mode == new_mode:
+		new_mode = modes[randi() % modes.size()]
+	mode = new_mode
+	get_tree().call_group("game", "queue_free")
+	setup_games()
+	
+
+func _on_ComboTimer_timeout():
+	miss()
+
+func setup_games():
+	var game = null
+	for i in mode:
+		game = Game.instance()
+		add_child(game)
+		game.wait_time = Constants.ModeWaitTime[mode]
+		game.scale = Vector2(Constants.ModeScale[mode]["x"], Constants.ModeScale[mode]["y"])
+		game.position = Vector2(Constants.ModePosition[mode][i]["x"], Constants.ModePosition[mode][i]["y"])
+		game.connect("score", self, "score")
+		game.connect("miss", self, "miss")
+		game.connect("wrong", self, "wrong")
+
+
+
+
 
 
