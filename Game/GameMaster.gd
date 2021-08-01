aaa7b1c8f046e9c7a0f01a4e428c1e2aedfc87ef
AdrKacz <adrien.kaczmarek@gmail.com>
2021-08-01 02:41:27 +0100

    Multi window

--- Game/GameMaster.gd
+++ Game/GameMaster.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-export (PackedScene) var Game
 export (PackedScene) var ComboScene
 
 var score = 0
@@ -8,15 +7,16 @@ var combo = 0
 var timer = 30
 # Number of active games
 var mode = 6
-var modes = [1, 2, 4, 6]
+var modes = [1, 2, 4]
 
 func _ready():
 	$ComboTimer.wait_time = Constants.max_combo_time # NOTE Can be infinite
-	setup_games()
+#	setup_games()
 
 func score():
 	score += Constants.ModeScore[mode] * max(1, combo)
 	$TimeTrial.add_time(1)
+	$PostEffect.play_shockwave()
 	add_combo()
 
 func miss():
@@ -49,28 +49,29 @@ func add_combo():
 		change_mode()
 
 func change_mode():
-	var new_mode = modes[randi() % modes.size()]
-	while mode == new_mode:
-		new_mode = modes[randi() % modes.size()]
-	mode = new_mode
-	get_tree().call_group("game", "queue_free")
-	setup_games()
+	pass
+#	var new_mode = modes[randi() % modes.size()]
+#	while mode == new_mode:
+#		new_mode = modes[randi() % modes.size()]
+#	mode = new_mode
+#	get_tree().call_group("game", "queue_free")
+#	setup_games()
 	
 
 func _on_ComboTimer_timeout():
 	miss()
 
-func setup_games():
-	var game = null
-	for i in mode:
-		game = Game.instance()
-		add_child(game)
-		game.wait_time = Constants.ModeWaitTime[mode]
-		game.scale = Vector2(Constants.ModeScale[mode]["x"], Constants.ModeScale[mode]["y"])
-		game.position = Vector2(Constants.ModePosition[mode][i]["x"], Constants.ModePosition[mode][i]["y"])
-		game.connect("score", self, "score")
-		game.connect("miss", self, "miss")
-		game.connect("wrong", self, "wrong")
+#func setup_games():
+#	var game = null
+#	for i in mode:
+#		game = Game.instance()
+#		add_child(game)
+#		game.wait_time = Constants.ModeWaitTime[mode]
+#		game.scale = Vector2(Constants.ModeScale[mode]["x"], Constants.ModeScale[mode]["y"])
+#		game.position = Vector2(Constants.ModePosition[mode][i]["x"], Constants.ModePosition[mode][i]["y"])
+#		game.connect("score", self, "score")
+#		game.connect("miss", self, "miss")
+#		game.connect("wrong", self, "wrong")
 
 
 
