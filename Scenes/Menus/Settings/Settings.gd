e60dc4595f9989b27c2599fe54d611a681f20293
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2023-05-01 11:22:31 +0100

    fix: settings display last options (#48)

--- Scenes/Menus/Settings/Settings.gd
+++ Scenes/Menus/Settings/Settings.gd
@@ -1,5 +1,15 @@
 extends Control
 
+func _ready():
+	var music_check_box: CheckBox = $MarginContainer/CenterContainer/VBoxContainer/MusicCheckBox
+	var sound_effects_check_box: CheckBox = $MarginContainer/CenterContainer/VBoxContainer/SoundEffectCheckBox
+	var vibrations_check_box: CheckBox = $MarginContainer/CenterContainer/VBoxContainer/VibrationsCheckBox
+	
+	music_check_box.set_pressed_no_signal(SoundManager.music_on)
+	sound_effects_check_box.set_pressed_no_signal(SoundManager.sound_effects_on)
+	vibrations_check_box.set_pressed_no_signal(Session.vibration_on)
+	
+
 func _on_exit_button_pressed():
 	Session.main_menu()
 
