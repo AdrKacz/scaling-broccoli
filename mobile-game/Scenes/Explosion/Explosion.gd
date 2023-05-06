b81196345168e69cca79e1631bc4b81a00d462ef
AdrKacz <adrien.kaczmarek@gmail.com>
2023-05-06 15:17:31 +0100

    feat: simplify animation

--- mobile-game/Scenes/Explosion/Explosion.gd
+++ mobile-game/Scenes/Explosion/Explosion.gd
@@ -1,18 +1,18 @@
 extends ColorRect
 
-var animation_duration: float
+@export var animation_duration: float = 1.0 
+
 func _ready():
 	visible = false
-	material.set_shader_parameter("time", 0.)
-	animation_duration = material.get_shader_parameter("animation_duration")
-	
-func update_explosion_time(time: float):
-	material.set_shader_parameter("time", time)
-	
+	material.set_shader_parameter("radius_percentage", 0.)
+
+func update_explosion_radius(radius_percentage: float):
+	material.set_shader_parameter("radius_percentage", radius_percentage)
+
 var tween: Tween
 func explode():
 	if tween:
 		tween.kill()
 	visible = true
-	tween = create_tween().bind_node(self)
-	tween.tween_method(update_explosion_time, 0., 1., animation_duration)
+	tween = create_tween().bind_node(self).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT_IN)
+	tween.tween_method(update_explosion_radius, 0., 1., animation_duration)
