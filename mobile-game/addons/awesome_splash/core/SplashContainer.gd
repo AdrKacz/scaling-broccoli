ab10707640ace973417dea9795c75095d36b0bd1
AdrKacz <adrien.kaczmarek@gmail.com>
2023-05-06 18:10:16 +0100

    feat: let you skip on you mobile

--- mobile-game/addons/awesome_splash/core/SplashContainer.gd
+++ mobile-game/addons/awesome_splash/core/SplashContainer.gd
@@ -19,10 +19,11 @@ func _ready():
 # type skip = SKIP_ONE_SCREEN_WHEN_CLICKED or SKIP_ALL_SCREEN_WHEN_CLICKED
 # Todo: update condition skip splash screen: when you click, when you press key...
 func _skip_awe_splash_by_event(event) -> bool:
-	return event is InputEventMouseButton \
-			and event.pressed \
-			and event.button_index == 1
-
+	print('EVENT')
+	print(event)
+	return event.is_pressed() \
+		and ((event is InputEventMouseButton and event.button_index == 1) \
+		or (event is InputEventScreenTouch))
 
 # Todo: move to other screen here:
 func _on_finished_all_splash_screen():
