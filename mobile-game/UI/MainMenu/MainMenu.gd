8bb4e7592d2ec49a553b67a7dda028c43d8e3041
AdrKacz <adrien.kaczmarek@gmail.com>
2023-05-16 09:20:33 +0100

    feat: add a challenge button in main menu

--- mobile-game/UI/MainMenu/MainMenu.gd
+++ mobile-game/UI/MainMenu/MainMenu.gd
@@ -4,10 +4,6 @@ signal on_screen
 func _ready():
 	emit_signal("on_screen")
 
-func _on_play_pressed():
-	Session.click()
-	Session.change_node_to(Session.GameMaster)
-
 func _on_leaderboard_button_pressed():
 	Session.click()
 	Session.change_node_to(Session.LeaderboardMenu)
@@ -15,3 +11,7 @@ func _on_leaderboard_button_pressed():
 func _on_settings_button_pressed():
 	Session.click()
 	Session.change_node_to(Session.SettingsMenu)
+
+func _on_arcade_pressed():
+	Session.click()
+	Session.change_node_to(Session.GameMaster)
