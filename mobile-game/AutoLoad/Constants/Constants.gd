6132c1af0bef97a720caffb7ed8b1d60b91d63ed
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2023-05-22 21:31:18 +0100

    feat: lose one heart each time you fail challenge (#76)

--- mobile-game/AutoLoad/Constants/Constants.gd
+++ mobile-game/AutoLoad/Constants/Constants.gd
@@ -7,6 +7,9 @@ const score_factor: int = 10
 
 const max_swaps: Array[int] = [5, 4, 3]
 
+const min_life = 0
+const max_life = 3
+
 var state_matches: bool = false
 var swap_delta: float:
 	get:
@@ -34,3 +37,46 @@ const State = {
 # Variables definitions
 var score: int = 0
 var combos_strike: int = 0
+
+var remaining_lives: int:
+	get:
+		return remaining_lives
+	set(value):
+		remaining_lives = clamp(value, min_life, max_life)
+		var config = ConfigFile.new()
+		config.set_value('parameters', 'remaining_lives', remaining_lives)
+		config.set_value('parameters', 'last_update_datetime', Time.get_datetime_string_from_system())
+		config.save("user://parameters.cfg")
+
+func _ready():
+	read_remaining_lives_from_memory()
+
+func read_remaining_lives_from_memory():
+	var config = ConfigFile.new()
+	var err = config.load("user://parameters.cfg")
+	if err != OK:
+		remaining_lives = max_life
+		return
+	var last_remaining_lives: int = config.get_value('parameters', 'remaining_lives', max_life)
+	if is_same_update_day(config.get_value('parameters', 'last_update_datetime')):
+		remaining_lives = last_remaining_lives
+	else:
+		remaining_lives = max_life
+
+func is_same_update_day(last_update_datetime) -> bool:
+	if last_update_datetime == null:
+		return false
+	var today_datetime_dict: Dictionary = Time.get_datetime_dict_from_system()
+	var last_update_datetime_dict: Dictionary = Time.get_datetime_dict_from_datetime_string(last_update_datetime, false)
+	if is_same_day(today_datetime_dict, last_update_datetime_dict):
+		return true
+	return false
+
+func is_same_day(date_a: Dictionary, date_b: Dictionary) -> bool:
+	if date_a.day != date_b.day:
+		return false
+	if date_a.month != date_b.month:
+		return false
+	if date_a.year != date_b.year:
+		return false
+	return true
