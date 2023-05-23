15557b1d35f751c5c39887ec4beb70d2536e4590
AdrKacz <adrien.kaczmarek@gmail.com>
2023-05-23 09:46:51 +0100

    feat: add completion challenge UI

--- mobile-game/Scenes/Menus/Challenge/Challenge.gd
+++ mobile-game/Scenes/Menus/Challenge/Challenge.gd
@@ -1,9 +1,25 @@
 extends CanvasLayer
 signal on_screen
 
+var hearts: MarginContainer
+var play_button: TextureButton
+var play_button_label: Label
+var info_label: Label
+var completion_label: Label
+
 func _ready():
+	hearts = $Control/MarginContainer/CenterContainer/VBoxContainer/Hearts
+	play_button = $Control/MarginContainer/CenterContainer/VBoxContainer/Play
+	play_button_label = $Control/MarginContainer/CenterContainer/VBoxContainer/Play/CenterContainer/Label
+	info_label = $Control/MarginContainer/CenterContainer/VBoxContainer/InfoLabel
+	completion_label = $Control/MarginContainer/CenterContainer/VBoxContainer/CompletionLabel
 	if Constants.remaining_lives == 0:
-		$Control/MarginContainer/CenterContainer/VBoxContainer/Play.queue_free()
+		play_button.visible = false
+	if Constants.challenge_completed:
+		play_button_label.text = "Play again"
+		info_label.visible = true
+		completion_label.visible = true
+		hearts.visible = false
 	emit_signal("on_screen")
 
 func _on_exit_button_pressed():
