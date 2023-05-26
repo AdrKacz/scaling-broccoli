dfbd246f322a79820e6fe16c4f09eab991d2e446
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2023-05-26 16:06:17 +0100

    82 reset streak when you miss a day (#85)
    * feat: UNTESTED reset streak after 1 day w\out win
    
    * feat: update memory

--- mobile-game/AutoLoad/Memory/Memory.gd
+++ mobile-game/AutoLoad/Memory/Memory.gd
@@ -52,22 +52,37 @@ func reset_challenge():
 	
 func read_challenge_from_memory():
 	var last_update_datetime: String = config.get_value('parameters', 'last_update_datetime', "")
-	if is_same_update_day(last_update_datetime):
-		remaining_lives = config.get_value('parameters', 'remaining_lives', max_life)
-		challenge_completed = config.get_value('parameters', 'challenge_completed', false)
+	if last_update_datetime == "":
+		reset_challenge()
+		lightning = 0
+	
+	var today_datetime: String = Time.get_datetime_string_from_system()
+	
+	var local_remaining_lives = config.get_value('parameters', 'remaining_lives', max_life)
+	var local_challenge_completed = config.get_value('parameters', 'challenge_completed', false) 
+	if is_same_day(last_update_datetime, today_datetime):
+		print('Last update same day')
+		remaining_lives = local_remaining_lives
+		challenge_completed = local_challenge_completed
+	elif is_same_day(get_next_day(last_update_datetime), today_datetime):
+		print('Last update yesterday')
+		if not local_challenge_completed:
+			print('Challenge was not completed')
+			lightning = 0 # challenge not succeeded yesterday
+		reset_challenge()
 	else:
+		print('Last update before yesterday')
+		lightning = 0 # streak lost
 		reset_challenge()
 
-func is_same_update_day(last_update_datetime: String) -> bool:
-	if last_update_datetime == "":
-		return false
-	var today_datetime_dict: Dictionary = Time.get_datetime_dict_from_system()
-	var last_update_datetime_dict: Dictionary = Time.get_datetime_dict_from_datetime_string(last_update_datetime, false)
-	if is_same_day(today_datetime_dict, last_update_datetime_dict):
-		return true
-	return false
+func get_next_day(date_string: String) -> String:
+	var date_unix: int = Time.get_unix_time_from_datetime_string(date_string)
+	date_unix += 86400 # number of seconds in a day
+	return Time.get_datetime_string_from_unix_time(date_unix)
 
-func is_same_day(date_a: Dictionary, date_b: Dictionary) -> bool:
+func is_same_day(date_string_a: String, date_string_b: String) -> bool:
+	var date_a: Dictionary = Time.get_datetime_dict_from_datetime_string(date_string_a, false)
+	var date_b: Dictionary = Time.get_datetime_dict_from_datetime_string(date_string_b, false)
 	if date_a.day != date_b.day:
 		return false
 	if date_a.month != date_b.month:
