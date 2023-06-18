90616c85d60d5fd976a49e32e60a97009715d7d2
AdrKacz <adrien.kaczmarek@gmail.com>
2023-06-18 13:10:14 +0100

    feat: read password

--- scripts/exports/create-export-preset.py
+++ scripts/exports/create-export-preset.py
@@ -1,1 +1,14 @@
-print('Create Export Preset')
+"""Create Export Preset"""
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="Create Export Preset",
+    description="Populate the export preset in mobile-game/")
+
+parser.add_argument('--path', type=str, required=True)
+parser.add_argument('--alias', type=str, required=True)
+parser.add_argument('--password', type=str, required=True)
+
+args = parser.parse_args()
+print(args.path, args.alias, args.password)
