af5e81523d5bc40d8e7dc05be3625b127a2b7cf9
AdrKacz <adrien.kaczmarek@gmail.com>
2023-06-18 14:03:47 +0100

    feat: update exports

--- scripts/exports/create-export-preset.py
+++ scripts/exports/create-export-preset.py
@@ -2,6 +2,18 @@
 
 import argparse
 
+def replace_config_values(input_file, output_file, key_value_pairs):
+    with open(input_file, 'r') as input_f:
+        with open(output_file, 'w') as output_f:
+            for line in input_f:
+                for key, value in key_value_pairs.items():
+                    if line.strip().startswith(key + '='):
+                        line = f'{key}="{value}"\n'
+                        break
+                output_f.write(line)
+config_file = './mobile-game/export_presets.cfg'
+template_config_file = './scripts/exports/export_presets.cfg'
+
 parser = argparse.ArgumentParser(
     prog="Create Export Preset",
     description="Populate the export preset in mobile-game/")
@@ -11,4 +23,11 @@ parser.add_argument('--alias', type=str, required=True)
 parser.add_argument('--password', type=str, required=True)
 
 args = parser.parse_args()
-print(args.path, args.alias, args.password)
+
+replacement_pairs = {
+    'keystore/release': args.path,
+    'keystore/release_user': args.alias,
+    'keystore/release_password': args.password
+}
+
+replace_config_values(template_config_file, config_file, replacement_pairs)
