6049217d7e04330c62415b52f6fa85ff3fcc0a19
AdrKacz <adrien.kaczmarek@gmail.com>
2024-06-07 10:43:28 +0100

    fix: directoy not found

--- exports/ios/ci_scripts/ci_post_clone.sh
+++ exports/ios/ci_scripts/ci_post_clone.sh
@@ -8,16 +8,8 @@ cd ../../../
 curl -LO https://github.com/godotengine/godot/releases/download/$VERSION-stable/Godot_v$VERSION-stable_macos.universal.zip
 unzip ./Godot_v$VERSION-stable_macos.universal.zip
 echo "Directory Contents:\n$(ls)"
-cd Godot.app/
-echo "Directory Contents:\n$(ls)"
-cd Contents/
-echo "Directory Contents:\n$(ls)"
-cd MacOS/
-echo "Directory Contents:\n$(ls)"
-alias godot="./Godot.app/Contents/MacOS/Godot"
+alias godot="Godot.app/Contents/MacOS/Godot"
 godot --version
-
-
 
 echo "Exporting iOS Production"
 cd ./mobile-game
