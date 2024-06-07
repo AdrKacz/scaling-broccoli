1fbe36678b12af39fd2e3056adc624e30ed5333e
AdrKacz <adrien.kaczmarek@gmail.com>
2024-06-07 10:35:07 +0100

    feat: download from source

--- exports/ios/ci_scripts/ci_post_clone.sh
+++ exports/ios/ci_scripts/ci_post_clone.sh
@@ -3,12 +3,15 @@ echo "Current Directory: $PWD"
 echo "Directory Contents:\n$(ls)"
 
 echo "Installing Godot"
-brew update
-brew install --cask godot
-# brew installs at '/Applications/Godot.app'
-alias godot="/Applications/Godot.app/Contents/MacOS/Godot"
+VERSION=4.2.2
+cd ../../../
+curl -LO https://github.com/godotengine/godot/releases/download/$VERSION-stable/Godot_v$VERSION-stable_macos.universal.zip
+unzip ./Godot_v$VERSION-stable_macos.universal.zip
+alias godot="./Godot.app/Contents/MacOS/Godot"
 godot --version
 
+
+
 echo "Exporting iOS Production"
-cd ../../../mobile-game
+cd ./mobile-game
 godot ---headless --export-release "iOS Production" ../exports/ios/scaling-broccoli.xcodeproj
