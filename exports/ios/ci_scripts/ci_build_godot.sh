0963bc800c2696d187cd883951dca23b7d5bd261
AdrKacz <adrien.kaczmarek@gmail.com>
2024-06-07 21:12:30 +0100

    fix: remove unused

--- exports/ios/ci_scripts/ci_build_godot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-VERSION=4.2.2-stable
-wget https://github.com/godotengine/godot/releases/download/$VERSION/godot-$VERSION.tar.xz
-tar xvf godot-$VERSION.tar.xz --strip-components=1
-brew install scons yasm
-scons --version
-scons platform=server tools=yes use_static_cpp=no -j 8
-mv ./bin/godot_server.osx.tools.64 ./bin/godot
