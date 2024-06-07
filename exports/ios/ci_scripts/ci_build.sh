b75af41d5bb19f0305a7314e28fb440f9338d2ba
AdrKacz <adrien.kaczmarek@gmail.com>
2024-06-07 10:16:39 +0100

    fix: update scripts

--- exports/ios/ci_scripts/ci_build.sh
+++ exports/ios/ci_scripts/ci_build.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/zsh
 echo "Current Directory: $PWD"
 echo "Directory Contents:\n$(ls)"
 
 echo "Installing Godot"
 brew update
 brew install --cask godot
+# brew installs at '/Applications/Godot.app'
+alias godot="/Applications/Godot.app/Contents/MacOS/Godot"
 godot --version
 
 echo "Exporting iOS Production"
-
 cd ../../../mobile-game
 godot ---headless --export-release "iOS Production" ../exports/ios/scaling-broccoli.xcodeproj
