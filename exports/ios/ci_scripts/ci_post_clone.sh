b8ce2454c6fc6133940a66dd3345dc4a33c68523
AdrKacz <adrien.kaczmarek@gmail.com>
2024-06-07 10:38:45 +0100

    fix: try debug export

--- exports/ios/ci_scripts/ci_post_clone.sh
+++ exports/ios/ci_scripts/ci_post_clone.sh
@@ -7,6 +7,13 @@ VERSION=4.2.2
 cd ../../../
 curl -LO https://github.com/godotengine/godot/releases/download/$VERSION-stable/Godot_v$VERSION-stable_macos.universal.zip
 unzip ./Godot_v$VERSION-stable_macos.universal.zip
+echo "Directory Contents:\n$(ls)"
+cd Godot.app/
+echo "Directory Contents:\n$(ls)"
+cd Contents/
+echo "Directory Contents:\n$(ls)"
+cd MacOS/
+echo "Directory Contents:\n$(ls)"
 alias godot="./Godot.app/Contents/MacOS/Godot"
 godot --version
 
