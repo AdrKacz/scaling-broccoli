5db833550740039d131986823134db8ea3c474bc
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2024-06-14 16:13:50 +0100

    feat: add localised price (#169)

--- mobile-game/UI/Store/Products/Product.gd
+++ mobile-game/UI/Store/Products/Product.gd
@@ -1,6 +1,8 @@
 extends PanelContainer
 signal pressed
 
+@export var product_id: String
+
 var disabled: bool:
 	get:
 		return $Panel.visible
@@ -10,3 +12,9 @@ var disabled: bool:
 func _on_button_pressed():
 	Session.click()
 	emit_signal("pressed")
+	
+func init():
+	if not product_id or not product_id in Apple.products:
+		visible = false # Hide as it cannot be used without a valid product id
+		return
+	$HBoxContainer/Price.text = Apple.products[product_id] # Set the localised price
